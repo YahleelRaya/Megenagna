bd46e53ecd49bcab20810c2b5acdae666035ef34
Yahleel Raya <yahleelxo100@gmail.com>
2024-07-20 15:03:09 -0400

    Connecting Mongodb to project

--- Mege_proj/Mege_app/urls.py
+++ Mege_proj/Mege_app/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-     path("Employer_Registration/", views.retrieve, name="Yahleel"),
+urlpatterns =[
+    # path("Employer_Registration/", views.retrieve, name="Yahleel"),
+     path('retrieve/<str:user_type>/', views.retrieve, name='retrieve'),
      path("submit/",views.submit,name="submit")
+     path('add/',views.add_user),
+     path('find/',views.get_all_users),
 ]
--- Mege_proj/Mege_app/views.py
+++ Mege_proj/Mege_app/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseBadRequest
 
 # Create your views here.
-def retrieve(request):
-    template= loader.get_template('Employer_registration/Employer_registration.html')
-    return HttpResponse(template.render())
-
+#def retrieveEmployer(request):
+   # template= loader.get_template('Employer_registration/Employer_registration.html')
+   # return HttpResponse(template.render())
+def retrieve(request, user_type):
+    if user_type == 'employer':
+        template_name = 'Employer_registration/Employer_registration.html'
+    elif user_type == 'worker':
+        template_name = 'Worker_registration/Worker_registration.html'
+    else:
+        return HttpResponseBadRequest("Invalid user type specified.")
+    context = {
+        'title': f'{user_type.capitalize()} Registration',
+        'message': f'Welcome to the {user_type.capitalize()} Registration Page',
+    }
+    return render(request, template_name, context)
 @csrf_exempt
 def submit(request):
     if request.method == 'POST':
